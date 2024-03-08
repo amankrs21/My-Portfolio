ac720594935d92c721ccb49ddb8b63665d0bac92
Aman Kumar Singh <amansingh844123@gmail.com>
2024-03-09 00:42:22 +0530

    Added Files and Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+import getLPTheme from "./components/getLPTheme.jsx";
+import ToggleColorMode from "./components/ToggleColorMode.jsx";
+
+// import Home from './pages/home/Home'
+// import PageNotFound from './components/PageNotFound.jsx';
+// import AboutUs from './pages/about/AboutUs.jsx';
+
+export default function App() {
+  const localMode = localStorage.getItem('mode');
+  const [mode, setMode] = useState(localMode ? localMode : 'dark');
+  const LPtheme = React.useMemo(() => createTheme(getLPTheme(mode)), [mode]);
+
+  const toggleColorMode = () => {
+    const newMode = mode === 'dark' ? 'light' : 'dark';
+    setMode(newMode);
+    localStorage.setItem('mode', newMode);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={LPtheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Header mode={mode} />
+        <div style={{ marginTop: 100 }}>
+          <Routes>
+            <Route path="*" element={<Navigate to="/404" />} />
+            <Route path='/' element={<Navigate to='/home' />} />
+            <Route path="/home" element={<Home />} />
+            <Route path='/about' element={<AboutUs />} />
+            <Route path='/contact' element={<ContactUs />} />
+            <Route path="/404" element={<PageNotFound />} />
+            <Route path="/" element={<PrivateRoutes />}>
+              <Route path='/todo' element={<Todos />} />
+              <Route path='/users' element={<Users />} />
+            </Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+      <div className='theme-mode' style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: "999" }}>
+        <ToggleColorMode style={{ backgroundColor: "transparent", border: "none", color: "white" }} mode={mode} toggleColorMode={toggleColorMode} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
   )
 }
-
-export default App
